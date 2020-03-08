22c49bdc9cf7b676f3200541639e17471378c489
Robin <1105080+openscript@users.noreply.github.com>
2020-03-08 21:29:15 +0100

    Add thesis directory

---
+++ native/thesis/1_noise_growth_addition.cpp
@@ -0,0 +1,50 @@
+#include "thesis.h"
+
+using namespace std;
+using namespace seal;
+
+void noise_growth_addition() {
+    print_example_banner("Noise Growth Addition");
+
+    EncryptionParameters parms(scheme_type::BFV);
+
+    size_t poly_modulus_degree = 2048;
+    parms.set_poly_modulus_degree(poly_modulus_degree);
+
+    parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
+
+    parms.set_plain_modulus(2097152);
+
+    auto context = SEALContext::Create(parms);
+
+    print_line(__LINE__);
+    cout << "Set encryption parameters and print" << endl;
+    print_parameters(context);
+    cout << endl;
+
+    KeyGenerator keygen(context);
+    PublicKey public_key = keygen.public_key();
+    SecretKey secret_key = keygen.secret_key();
+
+    Encryptor encryptor(context, public_key);
+    Evaluator evaluator(context);
+    Decryptor decryptor(context, secret_key);
+
+    int count = 20;
+    int initial = 1;
+
+    Plaintext initial_plain(to_string(initial));
+    Ciphertext current_encrypted;
+    encryptor.encrypt(initial_plain, current_encrypted);
+    cout << "Initialized with 0x" + initial_plain.to_string() << endl;
+    cout << "Initial noise budget:" << decryptor.invariant_noise_budget(current_encrypted) << " bits" << endl;
+
+    Plaintext decrypted_value;
+    
+    for( int i = 0; i < count; i = i + 1 ) {
+        evaluator.add_inplace(current_encrypted, current_encrypted);
+        cout << "Noise budget:" << decryptor.invariant_noise_budget(current_encrypted) << " bits" << endl;
+        decryptor.decrypt(current_encrypted, decrypted_value);
+        cout << "Result: 0x" + decrypted_value.to_string() << endl;
+    }
+}
