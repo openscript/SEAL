bea55beb662a9a484ea232163919d2c6e5de489c
Robin Bühler <public@openscript.ch>
2020-03-11 11:29:01 +0100

    Enhance noise growth example

--- native/thesis/1_noise_growth_addition.cpp
+++ native/thesis/1_noise_growth_addition.cpp
@@ -5,15 +5,14 @@ using namespace seal;
 
 void noise_growth_addition() {
     print_example_banner("Noise Growth Addition");
+    cout << "Allocated memory: " << MemoryManager::GetPool().alloc_byte_count() << endl;
 
     EncryptionParameters parms(scheme_type::BFV);
 
-    size_t poly_modulus_degree = 2048;
+    size_t poly_modulus_degree = 1024;
     parms.set_poly_modulus_degree(poly_modulus_degree);
-
     parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
-
-    parms.set_plain_modulus(2097152);
+    parms.set_plain_modulus(2048);
 
     auto context = SEALContext::Create(parms);
 
@@ -32,19 +31,25 @@ void noise_growth_addition() {
 
     int count = 20;
     int initial = 1;
+    int add = 2;
 
     Plaintext initial_plain(to_string(initial));
+    Plaintext add_plain(to_string(add));
+    Ciphertext add_encrypted;
     Ciphertext current_encrypted;
     encryptor.encrypt(initial_plain, current_encrypted);
+    encryptor.encrypt(add_plain, add_encrypted);
     cout << "Initialized with 0x" + initial_plain.to_string() << endl;
     cout << "Initial noise budget:" << decryptor.invariant_noise_budget(current_encrypted) << " bits" << endl;
 
     Plaintext decrypted_value;
     
     for( int i = 0; i < count; i = i + 1 ) {
-        evaluator.add_inplace(current_encrypted, current_encrypted);
+        evaluator.add_inplace(current_encrypted, add_encrypted);
         cout << "Noise budget:" << decryptor.invariant_noise_budget(current_encrypted) << " bits" << endl;
         decryptor.decrypt(current_encrypted, decrypted_value);
         cout << "Result: 0x" + decrypted_value.to_string() << endl;
     }
+
+    cout << "Allocated memory: " << MemoryManager::GetPool().alloc_byte_count() << endl;
 }
